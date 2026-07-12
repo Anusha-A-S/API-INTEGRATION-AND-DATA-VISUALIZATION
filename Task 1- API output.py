174c76d34917ff82797a79fdfea44d0c4e174a06
Anusha-A-S <anushasanningappanavar@gmail.com>
2026-07-12 13:39:23 +0530

    Add files via upload

---
+++ Task 1- API output.py
@@ -0,0 +1,24 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:\Users\anush\AppData\Local\Programs\Python\Python314\Task 1 input.py
+{'coord': {'lon': 76, 'lat': 13.5}, 'weather': [{'id': 803, 'main': 'Clouds', 'description': 'broken clouds', 'icon': '04d'}], 'base': 'stations', 'main': {'temp': 30.21, 'feels_like': 30.32, 'temp_min': 30.21, 'temp_max': 30.21, 'pressure': 1009, 'humidity': 43, 'sea_level': 1009, 'grnd_level': 915}, 'visibility': 10000, 'wind': {'speed': 5.69, 'deg': 281, 'gust': 6.76}, 'clouds': {'all': 78}, 'dt': 1783840972, 'sys': {'country': 'IN', 'sunrise': 1783816523, 'sunset': 1783862841}, 'timezone': 19800, 'id': 1267701, 'name': 'Karnataka', 'cod': 200}
+karataka weather broken clouds
+current temperature is 30.21
+current humidity is 43
+current wind speed 5.69
+{'coord': {'lon': 74.8667, 'lat': 32.7333}, 'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky', 'icon': '01d'}], 'base': 'stations', 'main': {'temp': 38.41, 'feels_like': 40.17, 'temp_min': 38.41, 'temp_max': 38.41, 'pressure': 996, 'humidity': 30, 'sea_level': 996, 'grnd_level': 957}, 'visibility': 10000, 'wind': {'speed': 1.61, 'deg': 276, 'gust': 2.11}, 'clouds': {'all': 7}, 'dt': 1783841030, 'sys': {'country': 'IN', 'sunrise': 1783814474, 'sunset': 1783865434}, 'timezone': 19800, 'id': 1269321, 'name': 'Jammu', 'cod': 200}
+Jmamu weather clear sky
+current temperature is 38.41
+current humidity is 30
+current wind speed is 1.61
+{'coord': {'lon': 74, 'lat': 26}, 'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky', 'icon': '01d'}], 'base': 'stations', 'main': {'temp': 35.48, 'feels_like': 37.2, 'temp_min': 35.48, 'temp_max': 35.48, 'pressure': 998, 'humidity': 37, 'sea_level': 998, 'grnd_level': 958}, 'visibility': 10000, 'wind': {'speed': 7.4, 'deg': 235, 'gust': 8.2}, 'clouds': {'all': 5}, 'dt': 1783840761, 'sys': {'country': 'IN', 'sunrise': 1783815589, 'sunset': 1783864735}, 'timezone': 19800, 'id': 1258899, 'name': 'Rajasthan', 'cod': 200}
+Rajasthan weather clear sky
+current temperature is 35.48
+current humidity is 37
+current wind speed is 7.4
+{'coord': {'lon': 76.5, 'lat': 10}, 'weather': [{'id': 804, 'main': 'Clouds', 'description': 'overcast clouds', 'icon': '04d'}], 'base': 'stations', 'main': {'temp': 30.72, 'feels_like': 34.5, 'temp_min': 30.72, 'temp_max': 30.72, 'pressure': 1009, 'humidity': 61, 'sea_level': 1009, 'grnd_level': 1006}, 'visibility': 10000, 'wind': {'speed': 2.93, 'deg': 299, 'gust': 4.9}, 'clouds': {'all': 100}, 'dt': 1783840986, 'sys': {'country': 'IN', 'sunrise': 1783816762, 'sunset': 1783862362}, 'timezone': 19800, 'id': 1267254, 'name': 'Kerala', 'cod': 200}
+kerala weather overcast clouds
+current temperature is 30.72
+current humidity is 61
+current wind speed is 2.93
